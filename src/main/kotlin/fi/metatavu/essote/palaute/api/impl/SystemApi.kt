f8dfd5a457dffb995f9280d5f2355912541ff854
villejuutila <cviju002@edu.xamk.fi>
2022-09-22 11:54:39 +0300

    Fixed ping endpoint...

--- src/main/kotlin/fi/metatavu/essote/palaute/api/impl/SystemApi.kt
+++ src/main/kotlin/fi/metatavu/essote/palaute/api/impl/SystemApi.kt
@@ -1,11 +1,10 @@
 package fi.metatavu.essote.palaute.api.impl
 
-import fi.metatavu.essote.palaute.api.utils.JwtUtils
 import fi.metatavu.spec.SystemApi
 import javax.ws.rs.core.Response
 
 class SystemApi: SystemApi, AbstractApi() {
     override fun ping(): Response {
-        return createOk(JwtUtils.createToken())
+        return createOk("pong")
     }
 }
